c4989d608994e084a753f20383965146cdfca281
bkawk <email.will.in.china@gmail.com>
2018-12-31 01:39:36 +0800

    show event

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const Promise = require('bluebird');
 
 exports.handler = function (event, context, callback) {
-    console.log(JSON.stringify(`Event: event`))
+    console.log(JSON.stringify(`Event: ${event}`))
     // Lambda Code Here
     // context.succeed('Success!')
     // context.fail('Failed!')
